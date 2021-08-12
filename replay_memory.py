a7396812f6b411c0609b658ec3d37c55549c9e3b
nnyx7 <nnyx77@gmail.com>
2021-08-12 20:15:36 +0300

    Add ReplayMemory

---
+++ replay_memory.py
@@ -0,0 +1,21 @@
+import random
+
+
+class ReplayMemory:
+    def __init__(self, capacity):
+        self.memory = []
+        self.__capacity = capacity
+        self.__add_index = 0
+
+    def add(self, experience):
+        if not(len(self.memory) == self.__capacity):
+            self.memory.append(experience)
+        else:
+            self.memory[self.__add_index] = experience
+            self.__add_index = (self.__add_index + 1) % self.__capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def can_provide_batch(self, batch_size):
+        return len(self.memory) >= batch_size
