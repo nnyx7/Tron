1f5626fa2302362afe1662af0e2ed5dca84ee8e8
nnyx7 <nnyx77@gmail.com>
2021-08-13 23:45:08 +0300

    Add grid method

--- helpers.py
+++ helpers.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from structs import Encodings
+
 
 def as_indexes(position, block_size):
     return (int(position[0] // block_size), int(position[1] // block_size))
@@ -23,3 +25,33 @@ def disassemble(batch):
         next_states.append(experience[3])
 
     return (np.array(states), np.array(actions), np.array(rewards), np.array(next_states))
+
+
+def grid(state, player_indexes, side_size):
+    grid_state = []
+    for i in range(side_size):
+        grid_state.append([])
+        for j in range(side_size):
+            grid_state[i].append(Encodings.EMPTY.value)
+
+    board_size = len(state)
+    offset = int(-(side_size - 1) // 2)
+
+    (x, y) = player_indexes
+
+    offset_x = offset
+    for i in range(side_size):
+        actual_x = x + offset_x
+
+        offset_y = offset
+        for j in range(side_size):
+            actual_y = y + offset_y
+            if (actual_x < 0 or actual_x >= board_size or actual_y < 0 or actual_y >= board_size):
+                grid_state[i][j] = Encodings.WALL_HIT.value
+            else:
+                grid_state[i][j] = state[actual_x][actual_y]
+            offset_y += 1
+
+        offset_x += 1
+
+    return grid_state
