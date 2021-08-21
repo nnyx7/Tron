19a1be3f70c3825a069eb79d26de7add055625c0
nnyx7 <nnyx77@gmail.com>
2021-08-21 19:00:25 +0300

    Make Agent choose best possible action

--- agent.py
+++ agent.py
@@ -2,6 +2,8 @@ import numpy as np
 import tensorflow as tf
 import random
 
+from helpers import best_possible_action
+
 
 class Agent:
     def __init__(self, model, num_actions, min_epsilon, max_epsilon, epsilon_decay_rate):
@@ -22,16 +24,16 @@ class Agent:
     def __explore(self):
         return random.randrange(self.num_actions)
 
-    def __exploit(self, state):
+    def __exploit(self, state, direction):
         state_tensor = tf.convert_to_tensor(state)
         state_tensor = tf.expand_dims(state_tensor, 0)
-        action_probabilities = self.model(state_tensor, training=False)
-        action = tf.argmax(action_probabilities[0]).numpy()
+        actions = self.model(state_tensor, training=False)
+        action = best_possible_action(actions, direction)
         return action
 
-    def action(self, state):
+    def action(self, state, direction):
         if random.random() > self.epsilon:
-            return self.__exploit(state)
+            return self.__exploit(state, direction)
         else:
             return self.__explore()
 
