0efc02ffff2efe140fd8cb8b6e883b3425f807a0
nnyx7 <nnyx77@gmail.com>
2021-08-23 12:31:35 +0300

    Rearrange functions in Game

--- tron.py
+++ tron.py
@@ -21,7 +21,9 @@ ENEMY_KEYS = {K_w: Direction.UP, K_s: Direction.DOWN,
 class Game:
     actions = [Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT]
 
-    def __init__(self, update_interval=INTERVAL, board_size=BOARD_SIZE, player_keys=PLAYER_KEYS, enemy_keys=ENEMY_KEYS,  ui=True, with_enemy=False):
+    def __init__(self, update_interval=INTERVAL, board_size=BOARD_SIZE,
+                 player_keys=PLAYER_KEYS, enemy_keys=ENEMY_KEYS,  ui=True, with_enemy=False):
+
         self.update_interval = update_interval
         self.player_keys = player_keys
         self.enemy_keys = enemy_keys
@@ -52,18 +54,11 @@ class Game:
             self.enemy = None
 
         if (self.ui):
-            self.enable_ui()
+            self.__enable_ui()
 
         self.result = Result.UNKNOWN
         self.steps_counter = 0
         self.reset()
-
-    def enable_ui(self):
-        self.ui = True
-        pygame.init()
-        self.surface = pygame.display.set_mode(self.screen_size)
-        self.player_block = pygame.image.load("assets/blue.png").convert()
-        self.enemy_block = pygame.image.load("assets/yellow.png").convert()
 
     def reset(self):
         self.result = Result.UNKNOWN
@@ -98,87 +93,20 @@ class Game:
 
             self.__update_ui()
 
-    def __update_state(self):
-        # Player
-        (prev_x, prev_y) = self.player.prev_head_indexes()
-        self.state[prev_x][prev_y] = Encodings.HIT.value
-
-        if not self.player.collision_with_wall():
-            (x, y) = self.player.head_indexes()
-            self.state[x][y] = Encodings.PLAYER_HEAD.value
-
-        if self.with_enemy:
-            # Enemy
-            (prev_x, prev_y) = self.enemy.prev_head_indexes()
-            self.state[prev_x][prev_y] = Encodings.HIT.value
-
-            if not self.enemy.collision_with_wall():
-                (x, y) = self.enemy.head_indexes()
-                self.state[x][y] = Encodings.ENEMY_HEAD.value
-
-    def __update_result(self):
-        if self.with_enemy:
-            has_player_lost = self.player.collision(
-                (self.enemy.x, self.enemy.y))
-            has_enemy_lost = self.enemy.collision(
-                (self.player.x, self.player.y))
-
-            if has_player_lost and has_enemy_lost:
-                self.result = Result.DRAW
-            elif has_player_lost:
-                self.result = Result.LOSE
-            elif has_enemy_lost:
-                self.result = Result.WIN
-        else:
-            if self.player.collision(enemy_positions=None):
-                self.result = Result.LOSE
-
-    def __display_result(self):
-        font = pygame.font.SysFont('arial', 30)
-        result = font.render(self.result.value, True, (255, 255, 255))
-
-        x = (self.surface.get_width() - result.get_width()) // 2
-        y = (self.surface.get_height() - result.get_height()) // 2
-
-        self.surface.blit(result, (x, y))
-        pygame.display.flip()
-
-    def __update_ui(self):
-        # Draw player
-        for i in range(self.player.length):
-            self.surface.blit(self.player_block,
-                              (self.player.x[i], self.player.y[i]))
-        if self.with_enemy:
-            # Draw enemy
-            for i in range(self.enemy.length):
-                self.surface.blit(self.enemy_block,
-                                  (self.enemy.x[i], self.enemy.y[i]))
-
-        pygame.display.flip()
-
-    def print_state(self):
-        state_string = ""
-        for j in range(len(self.state[0])):
-            for i in range(len(self.state)):
-                state_string += f'{self.state[i][j]} '
-            state_string += '\n'
-
-        print(state_string)
-
-    def __try_as_index(action):
-        try:
-            return Game.actions[int(action)]
-        except:
-            return action
-
     def step(self, action, enemy_action, wait=False):
+        def try_as_index(action):
+            try:
+                return Game.actions[int(action)]
+            except:
+                return action
+
         if self.result == Result.UNKNOWN:
-            action = Game.__try_as_index(action)
+            action = try_as_index(action)
             self.player.move(action, self.steps_counter == 0)
             self.player.progress()
 
             if self.with_enemy:
-                enemy_action = Game.__try_as_index(enemy_action)
+                enemy_action = try_as_index(enemy_action)
                 self.enemy.move(enemy_action, self.steps_counter == 0)
                 self.enemy.progress()
 
@@ -276,6 +204,80 @@ class Game:
 
                 enemy_action = enemy.action(self.state)
                 self.step(agent_action, enemy_action, wait=True)
+
+    def __print_state(self):
+        state_string = ""
+        for j in range(len(self.state[0])):
+            for i in range(len(self.state)):
+                state_string += f'{self.state[i][j]} '
+            state_string += '\n'
+
+        print(state_string)
+
+    def __enable_ui(self):
+        self.ui = True
+        pygame.init()
+        self.surface = pygame.display.set_mode(self.screen_size)
+        self.player_block = pygame.image.load("assets/blue.png").convert()
+        self.enemy_block = pygame.image.load("assets/yellow.png").convert()
+
+    def __update_state(self):
+        # Player
+        (prev_x, prev_y) = self.player.prev_head_indexes()
+        self.state[prev_x][prev_y] = Encodings.HIT.value
+
+        if not self.player.collision_with_wall():
+            (x, y) = self.player.head_indexes()
+            self.state[x][y] = Encodings.PLAYER_HEAD.value
+
+        if self.with_enemy:
+            # Enemy
+            (prev_x, prev_y) = self.enemy.prev_head_indexes()
+            self.state[prev_x][prev_y] = Encodings.HIT.value
+
+            if not self.enemy.collision_with_wall():
+                (x, y) = self.enemy.head_indexes()
+                self.state[x][y] = Encodings.ENEMY_HEAD.value
+
+    def __update_result(self):
+        if self.with_enemy:
+            has_player_lost = self.player.collision(
+                (self.enemy.x, self.enemy.y))
+            has_enemy_lost = self.enemy.collision(
+                (self.player.x, self.player.y))
+
+            if has_player_lost and has_enemy_lost:
+                self.result = Result.DRAW
+            elif has_player_lost:
+                self.result = Result.LOSE
+            elif has_enemy_lost:
+                self.result = Result.WIN
+        else:
+            if self.player.collision(enemy_positions=None):
+                self.result = Result.LOSE
+
+    def __display_result(self):
+        font = pygame.font.SysFont('arial', 30)
+        result = font.render(self.result.value, True, (255, 255, 255))
+
+        x = (self.surface.get_width() - result.get_width()) // 2
+        y = (self.surface.get_height() - result.get_height()) // 2
+
+        self.surface.blit(result, (x, y))
+        pygame.display.flip()
+
+    def __update_ui(self):
+        # Draw player
+        for i in range(self.player.length):
+            self.surface.blit(self.player_block,
+                              (self.player.x[i], self.player.y[i]))
+        if self.with_enemy:
+            # Draw enemy
+            for i in range(self.enemy.length):
+                self.surface.blit(self.enemy_block,
+                                  (self.enemy.x[i], self.enemy.y[i]))
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
