e87cc126bc11086c6a8be301ae0a77a67151d0b0
nnyx7 <nnyx77@gmail.com>
2021-08-24 12:51:20 +0300

    Fix ReplayMemory methods for sampling

--- replay_memory.py
+++ replay_memory.py
@@ -15,8 +15,10 @@ class ReplayMemory:
             self.__add_index = (self.__add_index + 1) % self.__capacity
 
     def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def sample_with_replacement(self, batch_size):
         return random.choices(self.memory, k=batch_size)
 
     def can_provide_batch(self, batch_size):
         return len(self.memory) >= batch_size
-        # return True
