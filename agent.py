44bc920f47c80cc5be8cf4c9e2c8dae214da5d25
nnyx7 <nnyx77@gmail.com>
2021-08-26 04:15:22 +0300

    Fixup Agent's rotated action

--- agent.py
+++ agent.py
@@ -1,13 +1,12 @@
+from structs import Direction
 import numpy as np
 import tensorflow as tf
 import random
 
-from helpers import best_possible_action
-
 
 class Agent:
-    __clockwise = {0: 0, 1: 2, 2: 3, 3: 1}
-    __rev_clockwise = {0: 0, 1: 3, 2: 1, 3: 2}
+    __mapping = {Direction.UP: [0, 1, 2, 3], Direction.LEFT: [3, 2, 0, 1],
+                 Direction.DOWN: [1, 0, 3, 2], Direction.RIGHT: [2, 3, 1, 0]}
 
     def __init__(self, model, num_actions, min_epsilon, max_epsilon, epsilon_decay_rate):
         self.model = model()
@@ -24,27 +23,28 @@ class Agent:
     def update_target_model(self):
         self.target_model.set_weights(self.model.get_weights())
 
-    def __explore(self):
-        return random.randrange(self.num_actions)
+    def __explore(self, direction):
+        action = random.randrange(self.num_actions)
+        if direction:
+            action = (action, Agent.__mapping[direction][action])
+        return action
 
     def __exploit(self, state, direction):
         state_tensor = tf.convert_to_tensor(state)
         state_tensor = tf.expand_dims(state_tensor, 0)
         actions = self.model(state_tensor, training=False)
 
-        action = tf.argmax(actions[0]).numpy()
-
-        if direction and direction != 'up':
-
-            if direction == 'right':
-                shift = 1
-            elif direction == 'down':
-                shift = 2
-            elif direction == 'left':
-                shift = 3
-
-            action = Agent.__rev_clockwise[(
-                Agent.__clockwise[action] + shift) % self.num_actions]
+        if direction:
+            actions = tf.argsort(actions[0].numpy())
+            if direction != Direction.UP:
+                actions = [(action.numpy(), Agent.__mapping[direction][action])
+                           for action in actions]
+                action = actions[self.num_actions - 1]
+            else:
+                action = (actions[self.num_actions - 1],
+                          actions[self.num_actions - 1])
+        else:
+            action = tf.argmax(actions[0]).numpy()
 
         return action
 
@@ -52,7 +52,7 @@ class Agent:
         if random.random() > self.epsilon:
             return self.__exploit(state, direction)
         else:
-            return self.__explore()
+            return self.__explore(direction)
 
     def decay_epsilon(self, episode):
         self.epsilon = self.min_epsilon + \
