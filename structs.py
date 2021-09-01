fc2e1fa9b363fa1f23ae3f1b818e00f3f352bc97
nnyx7 <nnyx77@gmail.com>
2021-09-01 17:04:51 +0300

    Add extra encodings

--- structs.py
+++ structs.py
@@ -31,3 +31,21 @@ class Encodings4(Enum):
     PLAYER_BODY = HIT
     ENEMY_HEAD = PLAYER_HEAD
     ENEMY_BODY = [0, 0, 1, 0]
+
+
+class Encodings5(Enum):
+    HIT = [0, 0, 0, 0, 1]
+    EMPTY = [1, 0, 0, 0, 0]
+    PLAYER_HEAD = [0, 1, 0, 0, 0]
+    PLAYER_BODY = [0, 0, 1, 0, 0]
+    ENEMY_HEAD = PLAYER_HEAD
+    ENEMY_BODY = [0, 0, 0, 1, 0]
+
+
+class Encodings(Enum):
+    HIT = 0
+    EMPTY = 1
+    PLAYER_HEAD = 2
+    PLAYER_BODY = 3
+    ENEMY_HEAD = 4
+    ENEMY_BODY = 5
